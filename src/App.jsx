8193266c523db07dea71b7c6a24a4a06307be049
Ashish Kumar <ashish2004kumar03@gmail.com>
2025-11-20 01:17:28 +0530

    Refactor App component by removing login logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
@@ -7,40 +6,18 @@ import HireMaasi from './components/HireMaasi'
 import ContactUs from './components/ContactUs'
 import MaasiProfile from './components/MaasiProfile'
 import Footer from './components/Footer'
-import LoginModal from './components/LoginModal'
+
+
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [showLoginModal, setShowLoginModal] = useState(false)
-
-  const handleLogin = () => {
-    setIsLoggedIn(true)
-    setShowLoginModal(false)
-  }
-
-  const handleLogout = () => {
-    setIsLoggedIn(false)
-  }
-
-  const openLoginModal = () => {
-    setShowLoginModal(true)
-  }
+ 
 
   return (
     <Router>
       <div className="App">
-        <Navbar 
-          isLoggedIn={isLoggedIn}
-          onLogout={handleLogout}
-          onLoginClick={openLoginModal}
-        />
-        
-        {showLoginModal && (
-          <LoginModal 
-            onLogin={handleLogin}
-            onClose={() => setShowLoginModal(false)}
-          />
-        )}
+        <Navbar/>
+
+      
 
         <Routes>
           <Route path="/" element={<Home />} />
